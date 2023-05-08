1a1ca0bed8e2f104b772546d980a8435f5ed0bcd
Ezra Marama <ezra.marama@gmail.com>
2023-05-08 13:33:26 -0500

    completed servlets bonus challenge

--- src/main/java/WelcomeServlet.java
+++ src/main/java/WelcomeServlet.java
@@ -25,10 +25,19 @@ public class WelcomeServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
         String username = req.getParameter("username");
+        String carName = req.getParameter("carType");
+        String carAwd = req.getParameter("carAwd");
         resp.setContentType("text/html");
 
-        PrintWriter out = resp.getWriter();
         out.println("<h2> Welcome, " + username + "!</h2>");
+        out.println("I like " + carName + "!");
+
+        if (carAwd.equalsIgnoreCase("carAwd")){
+            out.println("Wow i wish my car was AWD!");
+        } else {
+            out.println("You should consider getting an AWD car if you live in a winter state!");
+        }
     }
 }
