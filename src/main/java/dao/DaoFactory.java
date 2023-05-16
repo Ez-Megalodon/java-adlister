9b9575dd26f89bd25c43036ae9c8db8581758a29
Ezra Marama <ezra.marama@gmail.com>
2023-05-16 11:02:41 -0500

    finished jdbc-exercise

--- src/main/java/dao/DaoFactory.java
+++ src/main/java/dao/DaoFactory.java
@@ -2,12 +2,14 @@ package dao;
 
 import dao.Ads;
 
+import java.sql.SQLException;
+
 public class DaoFactory {
     private static Ads adsDao;
 
-    public static Ads getAdsDao() {
+    public static Ads getAdsDao() throws SQLException {
         if (adsDao == null) {
-            adsDao = new ListAdsDao();
+            adsDao = new MySQLAdsDao();
         }
         return adsDao;
     }
--- src/main/java/dao/MySQLAdsDao.java
+++ src/main/java/dao/MySQLAdsDao.java
@@ -4,14 +4,29 @@ import com.mysql.cj.jdbc.Driver;
 import config.Config;
 import models.Ad;
 
+import javax.swing.plaf.nimbus.State;
+import java.sql.DriverManager;
+
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MySQLAdsDao implements AdsDao{
+public class MySQLAdsDao implements Ads{
     private Connection connection;
 
-    public MySQLAdsDao(Connection connection, Config config) {
+    public MySQLAdsDao(){
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                    Config.getUrl(),
+                    Config.getUser(),
+                    Config.getPassword());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public MySQLAdsDao(Config config) {
         try {
             DriverManager.registerDriver(new Driver());
             connection = DriverManager.getConnection(
@@ -25,41 +40,19 @@ public class MySQLAdsDao implements AdsDao{
 
     @Override
     public List<Ad> all() {
-        // Get a reference to the authors Data Access Object
-        // so I can retrieve the list of authors
-//        Authors authorsDao = new AuthorsDao();
-        // retrieve all the authors in the database
-        // Store the list in a List called authors
-//        List<Author> authors = authorsDao.all();
-        // Then I create a list to store the quotes
         List<Ad> ads = new ArrayList<>();
         try {
             Statement statement = connection.createStatement();
-            // retrieve all the quotes from the database
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM adlister_db.ads");
-            // I start looping over the quotes I retrieved from the db
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM ads");
             while (resultSet.next()){
-                // For each quote, I get the author id
-                int author_id = resultSet.getInt("author_id");
-                // I create a an Author object that will hold the author information corresponding to the author id
-//                Author quoteAuthor = null;
-                // I loop over the list of all the authors
-//                for (Author author : authors){
-//                    // If I find a match between the author ids, I set the value of the author object
-//                    if (author.getId() == author_id){
-//                        quoteAuthor = author;
-//                    }
-//                }
-                // In the constructor, I add the Author object
                 Ad ad = new Ad(
                         resultSet.getLong("id"),
-                        resultSet.getLong("userID"),
+                        resultSet.getLong("user_id"),
                         resultSet.getString("title"),
                         resultSet.getString("description")
                 );
                 ads.add(ad);
             }
-
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -68,10 +61,17 @@ public class MySQLAdsDao implements AdsDao{
 
     @Override
     public Long insert(Ad ad) {
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("INSERT INTO ads (user_id, title, description) VALUES (" + ad.getUserId() + ", '" + ad.getTitle() + "','" + ad.getDescription() + "')", Statement.RETURN_GENERATED_KEYS);
+            ResultSet resultSet = statement.getGeneratedKeys();
+            if (resultSet.next()){
+                return resultSet.getLong(1);
+            }
+        } catch (SQLException e){
+            throw new RuntimeException(e);
+        }
         return null;
-    }
-
-    public MySQLAdsDao(){
     }
 
     public Connection getConnection() {
